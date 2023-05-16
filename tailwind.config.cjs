8d9fc74ce46e11c7f5fbecd9de07f891f7f7a191
Harald Sigvartsen <harald.sigvartsen@noaignite.com>
2023-05-16 14:58:22 +0200

    cleanup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		color: {
-			black: '#000814'
-		},
 		extend: {
 			colors: {
-				newLightYellow: '#ffd60a',
-				newYellow: '#ffc300',
-				newLightBlue: '#003566',
-				newDarkBlue: '#001d3d'
+				blue: '#fdf0d5',
+				black: '#000814'
 			}
 		}
 	},
